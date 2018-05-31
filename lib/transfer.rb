c387a0fea8b176db364f2d4bb9bb5fd81934676e
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-31 03:10:37 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,8 @@ class Transfer
   end
 
   def execute_transaction
+    if valid? == false 
+      "Transaction rejected. Please check your account balance."
     if @status == "complete"
     else
     @sender.balance -= @amount
