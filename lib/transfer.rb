8d3b307f55c9e8dd373adc0e5ed88756883b0d6b
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-31 03:34:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,12 +19,13 @@ class Transfer
   end
 
   def execute_transaction
+    binding.pry
     if @status == "pending"
     @sender.balance -= @amount
     @receiver.balance += @amount
     @status = "complete"
   elsif @status == "complete"
-    
+
   else @status == "rejected"
       "Transaction rejected. Please check your account balance."
 
